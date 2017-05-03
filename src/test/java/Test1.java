ebf2b053dd69c8befc02b48f46ba4783f19483a8
mgrecuero <mgrecuero@gmail.com>
2017-05-03 23:24:02 +0200

    Using dots for decimal separators

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -18,12 +18,9 @@ public class Test1 {
     @Test
     public void test1() {
         PriceCalculator priceCalculator = new PriceCalculator();
-        String[] expected1Array = {"* TK2372, 157,6 €", "* TK2659, 198,4 €", "* LH5909, 90,4 €"};
+        String[] expected1Array = {"* TK2372, 157.6 €", "* TK2659, 198.4 €", "* LH5909, 90.4 €"};
         //List<String> expected1 = Arrays.asList("* TK2372, 157,6 €", "* TK2659, 198,4 €", "* LH5909, 90,4 €");
         List<String> result1 = priceCalculator.calculatePrice("AMS", "FRA", 31, 1, 0, 0);
-        for (String res : result1) {
-            System.out.println("res: " + res);
-        }
         assertThat(result1, hasSize(3));
         assertThat(result1, containsInAnyOrder(expected1Array));
     }
@@ -32,7 +29,7 @@ public class Test1 {
     public void test2() {
         PriceCalculator priceCalculator = new PriceCalculator();
 
-        String[] expected2Array = {"* TK8891, 806 €", "* LH1085, 481,19 €"};
+        String[] expected2Array = {"* TK8891, 806 €", "* LH1085, 481.19 €"};
         //List<String> expected2 = Arrays.asList("* TK8891, 806 €", "* LH1085, 481,19 €");
         List<String> result2 = priceCalculator.calculatePrice("LHR", "IST", 15, 2, 1, 1);
         assertThat(result2, hasSize(2));
@@ -43,7 +40,7 @@ public class Test1 {
     public void test3() {
         PriceCalculator priceCalculator = new PriceCalculator();
 
-        String[] expected3Array = {"* IB2171, 909,09 €", "* LH5496, 1028,43 €"};
+        String[] expected3Array = {"* IB2171, 909.09 €", "* LH5496, 1028.43 €"};
         //List<String> expected3 = Arrays.asList("* IB2171, 909,09 €", "* LH5496, 1028,43 €");
         List<String> result3 = priceCalculator.calculatePrice("BCN", "MAD", 2, 1, 2, 0);
         assertThat(result3, hasSize(2));
